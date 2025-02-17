454cbdf467bccb3225f55deebe94e6f252fc6d71
Joseph Clement <jocl6895@colorado.edu>
2025-02-17 15:07:34 -0500

    set up for more generality

--- run/earlydata.sh
+++ run/earlydata.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# file name: firstcondor.sh
+
+UN=$4
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.458
 source /opt/sphenix/core/bin/setup_local.sh "/sphenix/user/jocl/projects/testinstall"
-export HOME=/sphenix/u/jocl
-export TESTINSTALL=/sphenix/user/jocl/projects/testinstall
-echo $LD_LIBRARY_PATH
-echo $PATH
+export HOME=/sphenix/u/$UN
+
 if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]; then
     cd $_CONDOR_SCRATCH_DIR
 else
@@ -20,10 +19,10 @@ for i in {1..10}; do
     mkdir -p trigout
     mkdir -p $SUBDIR
     mkdir -p lists
-    mkdir -p /sphenix/tg/tg01/jets/jocl/trigcount/${2}/
+    mkdir -p /sphenix/tg/tg01/jets/$UN/trigcount/${2}/
     mkdir -p ./dsts/$2/${2}_${UPLN}
-    cp -r /sphenix/user/jocl/projects/triggercountmodule/run/run_earlydata.C .
-    cp -r /sphenix/user/jocl/projects/triggercountmodule/run/lists/dst_calo_run2pp-000${2}.list ./lists/${2}.list
+    cp -r /sphenix/user/$UN/projects/triggercountmodule/run/run_earlydata.C .
+    cp -r /sphenix/user/$UN/projects/triggercountmodule/run/lists/dst_calo_run2pp-000${2}.list ./lists/${2}.list
     DSTFILE=`sed -n "${UPLN}"p "./lists/${2}.list"`
     if [ -z "${DSTFILE}" ]; then
 	exit 0
@@ -34,5 +33,5 @@ for i in {1..10}; do
     ls -larth
     mv $DSTFILE ./dsts/$2/${2}_${UPLN}.root
     root -b -q 'run_earlydata.C('${UPLN}',0,'${2}','${3}')'
-    cp trigout/* /sphenix/tg/tg01/jets/jocl/trigcount/${2}/
+    cp trigout/* /sphenix/tg/tg01/jets/$UN/trigcount/${2}/
 done
--- run/run_add.sh
+++ run/run_add.sh
@@ -8,13 +8,13 @@ SUBNAME="${BASENAME}.sub"
 
 echo "executable = addcommand.sh" > $PREFIX/$SUBNAME
 echo "concurrency_limits=CONCURRENCY_LIMIT_DEFAULT:500" >> $PREFIX/$SUBNAME
-echo "arguments = ${RN}" >> $PREFIX/$SUBNAME
+echo "arguments = ${RN} ${USER}" >> $PREFIX/$SUBNAME
 echo "priority = 0" >> $PREFIX/$SUBNAME
 echo "output = output/out/output_${BASENAME}_\$(Process).out" >> $PREFIX/$SUBNAME
 echo "should_transfer_files   = IF_NEEDED" >> $PREFIX/$SUBNAME
 echo "when_to_transfer_output = ON_EXIT" >> $PREFIX/$SUBNAME
 echo "error = output/err/error_${BASENAME}_\$(Process).err" >> $PREFIX/$SUBNAME
-echo "log = /tmp/jocl_${BASENAME}.log" >> $PREFIX/$SUBNAME
+echo "log = /tmp/${USER}_${BASENAME}.log" >> $PREFIX/$SUBNAME
 echo "queue 1" >> $PREFIX/$SUBNAME
 
 condor_submit $PREFIX/$SUBNAME
